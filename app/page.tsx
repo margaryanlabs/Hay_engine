f612d1b7e447d02478e99f5935120217e267d50c
margaryanlabs <margaryanlabs@gmail.com>
2026-09-01 16:30:49 +0400

    design: switch home to landing v2

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import LandingPage from "@/components/LandingPage";
+import LandingPageV2 from "@/components/LandingPageV2";
 
 export default function Home(){
-  return <LandingPage/>;
+  return <LandingPageV2/>;
 }
