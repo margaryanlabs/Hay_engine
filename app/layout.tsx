8bbd7cc74ef64a9e134b54abfc3756af02e5834d
margaryanlabs <margaryanlabs@gmail.com>
2026-09-01 16:30:38 +0400

    design: wire landing v2 styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./landing.css";
+import "./landing-v2.css";
 import "./creator.css";
 import "./marketing.css";
 import "./voice-lab.css";
