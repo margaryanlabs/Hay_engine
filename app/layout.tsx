8578784de177c47257f0569f67fd11c22f23455d
margaryanlabs <margaryanlabs@gmail.com>
2026-09-03 15:51:47 +0400

    Load correction consent control styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,7 @@ import "./native-benchmark.css";
 import "./developer.css";
 import "./pronunciation-console.css";
 import "./correction-lab.css";
+import "./correction-consent.css";
 import "./quality.css";
 import "./login.css";
 
